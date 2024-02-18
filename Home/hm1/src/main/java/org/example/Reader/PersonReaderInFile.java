df0818bbeb8f414983a0dee57da6dab57e05923a
Alexjuzz <juzzle@mail.ru>
2024-02-18 20:44:47 +0300

    удаление лишних фалов из гита

--- Home/hm1/src/main/java/org/example/Reader/PersonReaderInFile.java
+++
@@ -1,7 +0,0 @@
-package org.example.Reader;
-
-import org.example.Person;
-
-public interface PersonReaderInFile {
-    Person readPerson(String path);
-}
--- Home/hm1/src/main/java/org/example/Writer/SavePersonFormat.java
+++
@@ -1,8 +0,0 @@
-package org.example.Writer;
-
-import org.example.Person;
-
-public interface SavePersonFormat {
-    boolean saveToFilePerson(Person pesrson, String pathDir);
-
-}
