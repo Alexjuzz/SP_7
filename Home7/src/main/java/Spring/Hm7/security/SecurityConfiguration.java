a86f57dbc745ece025da08c75c8654526975e75d
Alexjuzz <juzzle@mail.ru>
2024-02-20 01:18:53 +0300

    отключены все методы AbstractHttpConfigurer. можно добавить пользователя но нельзя добавить задачу.

--- Home7/src/main/java/Spring/Hm7/security/SecurityConfiguration.java
+++ Home7/src/main/java/Spring/Hm7/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +13,14 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         return security.authorizeHttpRequests(registry -> registry
-
-//                .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN")
-//                .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN")
-//                .requestMatchers(HttpMethod.GET, "/task/**").permitAll()
-//                .requestMatchers(HttpMethod.GET, "/user/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "task/**").hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/task/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/user/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/").permitAll()
                 .requestMatchers(HttpMethod.POST, "/").permitAll()
                 .anyRequest().permitAll()
-        ).formLogin(Customizer.withDefaults()).csrf(Customizer.withDefaults()).build();
+        ).formLogin(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable).build();
     }
 }
